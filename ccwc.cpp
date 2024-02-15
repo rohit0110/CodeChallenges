819ec3792cbc0ee1a060debed98fe97bddf5a3a5
rohit0110 <rohit7rathore15@gmail.com>
2024-02-16 04:35:38 +0530

    Add functionality to take input from terminal and work if no options provided, default all

--- ccwc.cpp
+++ ccwc.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdio>
 
 using namespace std;
 
@@ -9,6 +10,7 @@ int numOfBytes(ifstream &file) {
     int bytes = 0;
     file.seekg(0,ios::end);
     bytes = file.tellg();
+    file.seekg(0, ios::beg);
     return bytes;
 }
 
@@ -19,6 +21,7 @@ int numOfLines(ifstream &file) {
     while(getline(file,line)) {
         lines++;
     }
+    file.seekg(0, ios::beg);
     return lines;
 }
 
@@ -29,6 +32,7 @@ int numOfWords(ifstream &file) {
     while(file >> word) {
         words++;
     }
+    file.seekg(0, ios::beg);
     return words;
 }
 
@@ -39,6 +43,7 @@ int numOfChars(ifstream &file) {
     while(file.get(ch)) {
         chars++;
     }
+    file.seekg(0, ios::beg);
     return chars;
 }
 
@@ -55,7 +60,58 @@ int main(int argc, char* argv[]) {
         cerr << "Usage: " << argv[0] << "<-l | -w | -c | -m> <filename>" << endl;
         return 1;
     }
-
+    if(argc == 2 && cin.peek()) {
+        if(!checkInput(string(argv[1]))) { //if not a valid input
+            cerr << "Usage: " << argv[0] << "<-l | -w | -c | -m> <filename>" << endl;
+            return 1;
+        }
+        char option = string(argv[1])[1];
+        string filename = "inputFile.txt";
+        ofstream outputFile(filename);
+        if (!outputFile)
+        {
+            cerr << "Error creating the file " << filename << endl;
+            return 1;
+        }
+        string inputText;
+        while(getline(cin,inputText)){
+            outputFile << inputText <<endl;
+        }
+        outputFile.close();
+        ifstream inputfile(filename);
+        if(!inputfile.is_open()) {
+            cerr << "Error: Cant open file" << filename << endl;
+            return 1;
+        }
+        switch(option) {
+            case 'l':   cout<<numOfLines(inputfile)<<endl;
+                        break;
+            case 'c':   cout<<numOfBytes(inputfile)<<endl;
+                        break;
+            case 'w':   cout<<numOfWords(inputfile)<<endl;
+                        break;
+            case 'm':   cout<<numOfChars(inputfile)<<endl;
+                        break;
+            default:    break;
+        }
+        inputfile.close();
+        remove("inputFile.txt");
+        return 0;
+    }
+    // STEP 5: All options
+    else if(argc == 2) {
+        ifstream inputfile(argv[1]);
+        if(!inputfile.is_open()) {
+            cerr << "Error: Cant open file" << argv[2] << endl;
+            return 1;
+        }
+        cout<<numOfBytes(inputfile)<<" "<<numOfLines(inputfile)<<" "<<numOfWords(inputfile)<<" "<<argv[1]<<endl;
+        inputfile.close();
+        return 0;
+    }
+    //STEP 6: No File Input
+    
+    
     //STEP 1,2,3,4: Correct parameters are passed
     if(argc == 3) {
         if(!checkInput(string(argv[1]))) { //if not a valid input
@@ -73,15 +129,16 @@ int main(int argc, char* argv[]) {
         switch(option) {
             case 'l':   cout<<numOfLines(inputfile)<<" "<<argv[2]<<endl;
                         break;
-            case 'c':   cout<<numOfBytes(inputfile)<<" "<<argv[2]<<endl;;
+            case 'c':   cout<<numOfBytes(inputfile)<<" "<<argv[2]<<endl;
                         break;
-            case 'w':   cout<<numOfWords(inputfile)<<" "<<argv[2]<<endl;;
+            case 'w':   cout<<numOfWords(inputfile)<<" "<<argv[2]<<endl;
                         break;
-            case 'm':   cout<<numOfChars(inputfile)<<" "<<argv[2]<<endl;;
+            case 'm':   cout<<numOfChars(inputfile)<<" "<<argv[2]<<endl;
                         break;
             default:    break;
         }
         inputfile.close();
+        return 0;
     }
     return 0;
 }
